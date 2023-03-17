3ddedd30a4d1ef27a8255b9c66ae9937aacb9385
HRA! <hra@mvj.biglobe.ne.jp>
2023-03-18 06:57:52 +0900

    Added

---
+++ sample_program/003_z80dk/2023_03_12_DEVCON3version/001_iotget/iotget.c
@@ -0,0 +1,28 @@
+// --------------------------------------------------------------------
+//	IOTGET for MSX0 (2023/3/12 DEVCON3 version)
+//
+//	ex)
+//		analog_level = iotget( "sample/analog" );
+// --------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <string.h>
+
+#define IOT_PORT	8
+
+int iotget( const char *p_device_path ) {
+	int l;
+	unsigned char r[2];
+
+	l = strlen( p_device_path );
+	outp( IOT_PORT, 0xAC );
+	outp( IOT_PORT, 1 );
+	outp( IOT_PORT, l );
+	for( i = 0; i < l; i++ ) {
+		outp( IOT_PORT, *p_device_path );
+		p_device_path++;
+	}
+	r[0] = inp(IOT_PORT);
+	r[1] = inp(IOT_PORT);
+	return *((int*) r );
+}
---
+++ sample_program/003_z80dk/2023_03_12_DEVCON3version/001_iotget/iotget.h
@@ -0,0 +1,13 @@
+// --------------------------------------------------------------------
+//	IOTGET for MSX0 (2023/3/12 DEVCON3 version)
+//
+//	ex)
+//		analog_level = iotget( "sample/analog" );
+// --------------------------------------------------------------------
+
+#ifndef __IOTGET_H__
+#define __IOTGET_H__
+
+int iotget( const char *p_device_path );
+
+#endif
